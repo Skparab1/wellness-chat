d5f2b12d60f438df2ce1f6c0d0c12d4d90024964
skparab1 <skparab1@gmail.com>
2022-11-18 22:07:05 -0800

    added linking between people

--- html-controller.js
+++ html-controller.js
@@ -1,7 +1,33 @@
 async function connectperson(){
+  // ok absically we have an id right
+  linking = true;
+
+
+  // and then put in the person
+  (async () => {
+    fetch((`https://pst652.deta.dev/?ADDLINKER=${id}`))
+      .then(response => {
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+      })
+  })();
 
 }
 
+function clearlinker(){
+  (async () => {
+    fetch((`https://pst652.deta.dev/?LINKCLEAR`))
+      .then(response => {
+          return response.json();
+      })
+      .then(data => {
+          console.log(data);
+      })
+  })();
+}
+
 async function sendmsg(){
-  
+
 }
--- index.js
+++ index.js
@@ -3,12 +3,62 @@
 const sleep = ms => new Promise(res => setTimeout(res, ms));
 
 
+// universal random id
+var pname = 'skparab1';
+var randnum = Math.floor(Math.random()*1000);
+
+var linking = false;
+var gottenonline = [];
+
+var id = pname+'00'+randnum;
+console.log(id);
+
 (async () => {
 
   while (true){
     
     // we can have the fetcher in here
+
+    if (linking){ // then we need to poll and stuff
+
+            // lets get a time 
+      var d = new Date();
+      if (d.getSeconds() % 5 == 0){ // check every 10 sec to link
+        (async () => {
+          fetch((`https://pst652.deta.dev/?CHATGET=linker`))
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+                gottenonline = data.messages;
+
+                // now we can process
+                if (gottenonline.length >= 2){ // verry likely we have a connection
+
+                  let u = 0;
+                  while (u < gottenonline.length){
+                    if (gottenonline[u] != id){ // not urself
+                      // we have a target
+                      linking = false;
+                      alert('connected with '+gottenonline[u]);
+
+                      // now clear the thing if u have the higher id
+                      let otherid = parseInt(gottenonline[u].split('00')[1]);
+                      console.log('otherid'+otherid+' ourid'+randnum);
+                      if (randnum > otherid){ // we can clear
+                        clearlinker();
+                      }
+
+                    }
+                    u += 1;
+                  }
+                }
+            })
+        })();
+      }
+    }
     
-    await sleep(2);
+    await sleep(1000); // garuntee not to call 2 times
   }
 })();
