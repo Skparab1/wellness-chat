9d4e8242e138e557a5e5fc3298ce74f6bb2fc7ac
skparab1 <skparab1@gmail.com>
2022-11-18 21:29:33 -0800

    the base there u go ritam

---
+++ html-controller.js
@@ -0,0 +1,7 @@
+async function connectperson(){
+
+}
+
+async function sendmsg(){
+  
+}
---
+++ index.js
@@ -0,0 +1,14 @@
+// wellness chat
+
+const sleep = ms => new Promise(res => setTimeout(res, ms));
+
+
+(async () => {
+
+  while (true){
+    
+    // we can have the fetcher in here
+    
+    await sleep(2);
+  }
+})();
